8397d5b2a9e9d5ae1db2ab96efdf996c1d124792
Gustavo de Souza <cxzaqwed@users.noreply.github.com>
2018-03-06 00:14:02 -0300

    Update editorMain.java

--- editorMain.java
+++ editorMain.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author gusta
- */
 import java.awt.Color;
 import javax.swing.*;
 
 public class editorMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
 
         JFrame janela = new JFrame("POP");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
